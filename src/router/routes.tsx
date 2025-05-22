b6693d17e95155c9129401a832dabdf84e35bc97
Matheus Gama <matheus97gama@gmail.com>
2025-05-22 11:20:36 -0300

    Update routes.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
+import { App } from '../App';
 import { Home } from '../pages/Home';
 import { IssuesPage } from '../pages/IssuesPage';
 import { Issue } from '../pages/IssuePage';
